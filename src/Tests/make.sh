38a80b06c8d3d5b97d4bc87ea612ece2f4e60692
fundamental <mark.d.mccurry@gmail.com>
2009-06-20 19:22:38 -0400

    Insured that Tests/make.sh will not fail during linking

--- src/Tests/make.sh
+++ src/Tests/make.sh
@@ -1,2 +1,5 @@
+cd ../
+make
+cd Tests
 cxxtestgen.py --error-printer -o runner.cpp SampleTest.h EchoTest.h
 g++ -g -o runner runner.cpp ../Samples/AuSample.o ../Samples/Sample.o ../Effects/Echo.o ../Effects/Effect.o ../Controls/Control.o ../Controls/DelayCtl.o 
