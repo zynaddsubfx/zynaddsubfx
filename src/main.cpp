68e595e03dbf01bd561fc74f910547fae87c3332
fundamental <mark.d.mccurry@gmail.com>
2010-01-14 16:16:25 -0500

    UI: Corrected behavior for "-U" launch flag
    Bug reported on KVR forum by pianolivier.
    ZynAddSubFX tried to connect to X11 server even with "-U" flag because it
    prematurely allocated the GUI before reading command line flags.
    Fixed.

--- src/main.cpp
+++ src/main.cpp
@@ -351,9 +351,6 @@ void initprogram()
 #if (defined(NONEMIDIIN) || (defined(VSTMIDIIN)))
     Midi = new NULLMidiIn();
 #endif
-#ifndef DISABLE_GUI
-    ui = new MasterUI(master, &Pexitprogram);
-#endif
 }
 
 /*
@@ -655,10 +652,6 @@ int main(int argc, char *argv[])
         }
         else {
             master->applyparameters();
-#ifndef DISABLE_GUI
-            if(noui == 0)
-                ui->refresh_master_ui();
-#endif
             cout << "Master file loaded." << endl;
         }
     }
@@ -673,10 +666,6 @@ int main(int argc, char *argv[])
         }
         else {
             master->part[loadtopart]->applyparameters();
-#ifndef DISABLE_GUI
-            if(noui == 0)
-                ui->refresh_master_ui();
-#endif
             cout << "Instrument file loaded." << endl;
         }
     }
@@ -701,8 +690,12 @@ int main(int argc, char *argv[])
     //      setregid(getuid(),getuid());
     #endif
     */
-    if(noui == 0)
+#ifndef DISABLE_GUI
+    if(noui == 0) {
+        ui = new MasterUI(master, &Pexitprogram);
         pthread_create(&thr3, NULL, thread3, NULL);
+    }
+#endif
 
     pthread_create(&thr4, NULL, thread4, NULL);
 #ifdef WINMIDIIN
