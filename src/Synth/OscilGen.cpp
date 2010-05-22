e7ec4d407d57e49e7f3416737dfec6433c0ba169
fundamental <mark.d.mccurry@gmail.com>
2010-05-22 13:55:15 -0400

    OscilGen: fix custom wave case

--- src/Synth/OscilGen.cpp
+++ src/Synth/OscilGen.cpp
@@ -1334,6 +1334,9 @@ base_func getBaseFunction(unsigned char func)
     if(!func)
         return NULL;
 
+    if(func == 127) //should be the custom wave
+        return NULL;
+
     func--;
     assert(func < 13);
     base_func functions[] = {
