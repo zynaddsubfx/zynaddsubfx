zynaddsubfx/zynaddsubfx

commit 7592cde773d62a74985c7d5e929504627ca0b731

Fixing Envelopes not going to zero
Min value updated from -40dB to -400dB

src/Synth/Envelope.cpp
@@ -182,7 +182,7 @@ REALTYPE Envelope::envout_dB()
         if(out > 0.001)
             envoutval = rap2dB(out);
         else
-            envoutval = -40.0;
+            envoutval = MIN_ENVELOPE_DB;
     }
     else
         out = dB2rap(envout());

src/Params/EnvelopeParams.h
@@ -28,7 +28,7 @@
 #include "Presets.h"
 
 #define MAX_ENVELOPE_POINTS 40
-#define MIN_ENVELOPE_DB -40
+#define MIN_ENVELOPE_DB -400
 
 class EnvelopeParams:public Presets
 {

commit ca0dafe866fca151e6807acc07857c7894242046

Env: fixing envelope scaling
Fixing bug introduced in 7592cde773d62a74985c7d5e929504627ca0b731

src/Synth/Envelope.cpp
@@ -57,7 +57,7 @@ Envelope::Envelope(EnvelopeParams *envpars, REALTYPE basefreq)
 
         switch(mode) {
         case 2:
-            envval[i] = (1.0 - envpars->Penvval[i] / 127.0) * MIN_ENVELOPE_DB;
+            envval[i] = (1.0 - envpars->Penvval[i] / 127.0) * -40;
             break;
         case 3:
             envval[i] =