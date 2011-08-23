56f5925750ac96460b2696c8497c68070f6feb96
fundamental <mark.d.mccurry@gmail.com>
2011-08-22 20:01:02 -0400

    cmake: setting up quasi-official build server

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -8,6 +8,6 @@ set(CTEST_PROJECT_NAME "ZynAddSubFX")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "my.cdash.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=ZynAddSubFX")
+set(CTEST_DROP_SITE "fundamental-code.com")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=ZynAddSubFX")
 set(CTEST_DROP_SITE_CDASH TRUE)
