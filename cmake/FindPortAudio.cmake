1888716f4aef9082ea7657bb4b18c87579961793
fundamental <mark.d.mccurry@gmail.com>
2011-08-27 12:52:43 -0400

    Cmake: fixing portaudio compile issues
    - case sensitive bug fix

--- cmake/FindPortAudio.cmake
+++ cmake/FindPortAudio.cmake
@@ -2,18 +2,18 @@
 
 
 include(LibFindMacros)
-libfind_pkg_check_modules(PortAudio portaudio-2.0>=19)
+libfind_pkg_check_modules(PORTAUDIO portaudio-2.0>=19)
 
-find_path(PortAudio_INCLUDE_DIR
+find_path(PORTAUDIO_INCLUDE_DIR
     NAMES portaudio.h
     PATHS ${PortAudio_INCLUDE_DIRS}
     )
 
-find_library(PortAudio_LIBRARY
+find_library(PORTAUDIO_LIBRARY
     NAMES portaudio
-    PATHS ${Portaudio_LIBRARY_DIRS}
+    PATHS ${PortAudio_LIBRARY_DIRS}
     )
 
-set(PortAudio_PROCESS_INCLUDES PortAudio_INCLUDE_DIR)
-set(PortAudio_PROCESS_LIBS PortAudio_LIBRARY)
-libfind_process(PortAudio)
+set(PORTAUDIO_PROCESS_INCLUDES PORTAUDIO_INCLUDE_DIR)
+set(PORTAUDIO_PROCESS_LIBS PORTAUDIO_LIBRARY)
+libfind_process(PORTAUDIO)
