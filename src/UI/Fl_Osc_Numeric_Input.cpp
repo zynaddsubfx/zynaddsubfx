fb3177f1d7e307c4749ef1772479d9f15e854a04
fundamental <mark.d.mccurry@gmail.com>
2015-06-18 14:51:15 -0400

    ConfigUI: Fix SampleRate/Buffersize

--- src/UI/Fl_Osc_Numeric_Input.cpp
+++ src/UI/Fl_Osc_Numeric_Input.cpp
@@ -3,6 +3,7 @@
 Fl_Osc_Numeric_Input::Fl_Osc_Numeric_Input(int X, int Y, int W, int H, const char *label)
     :Fl_Input(X,Y,W,H, label), Fl_Osc_Widget(this)
 {
+    callback(numeric_callback);
 }
 
 Fl_Osc_Numeric_Input::~Fl_Osc_Numeric_Input(void)
@@ -24,5 +25,12 @@ void Fl_Osc_Numeric_Input::OSC_value(int i)
     char buf[128];
     snprintf(buf, 128, "%d", i);
     value(buf);
-    printf("Osc value here\n");
+}
+
+void Fl_Osc_Numeric_Input::numeric_callback(Fl_Widget *w)
+{
+    auto &ww = *(Fl_Osc_Numeric_Input *)w;
+    int x = atoi(ww.value());
+    if(x)
+        ww.oscWrite(ww.ext, "i", x);
 }
