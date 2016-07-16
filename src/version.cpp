58b8c69845cde0ab303485e6ecf0c1732076f1b7
Johannes Lorenz <johannes89@ist-einmalig.de>
2016-07-16 10:30:56 +0200

    Add forgotten license to new files.

--- src/version.cpp
+++ src/version.cpp
@@ -1,3 +1,16 @@
+/*
+  ZynAddSubFX - a software synthesizer
+
+  version.cpp - implementation of version_type class
+  Copyright (C) 2016 Johannes Lorenz
+  Author: Johannes Lorenz
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+*/
+
 #include <iostream>
 
 #include "version.h"
