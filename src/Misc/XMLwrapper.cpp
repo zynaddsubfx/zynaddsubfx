60de7b194f1d27263a032546b5cf84321c62493a
Micky <yun_chan4@hotmail.com>
2019-03-04 23:08:08 +0200

    updated compatibility with mxml 3.0

--- src/Misc/XMLwrapper.cpp
+++ src/Misc/XMLwrapper.cpp
@@ -505,7 +505,7 @@ int XMLwrapper::getparbool(const string &name, int defaultpar) const
 
 void XMLwrapper::getparstr(const string &name, char *par, int maxstrlen) const
 {
-    int* set_Space = 1;
+    int set_Space = 1;
     ZERO(par, maxstrlen);
     mxml_node_t *tmp = mxmlFindElement(node,
                                        node,
@@ -523,9 +523,9 @@ void XMLwrapper::getparstr(const string &name, char *par, int maxstrlen) const
         snprintf(par, maxstrlen, "%s", mxmlGetElement(mxmlGetFirstChild(tmp)));
         return;
     }
-    if ((mxmlGetType(mxmlGetFirstChild(tmp)) == MXML_TEXT) && (mxmlGetText(mxmlGetFirstChild(tmp), set_Space) != NULL))
+    if ((mxmlGetType(mxmlGetFirstChild(tmp)) == MXML_TEXT) && (mxmlGetText(mxmlGetFirstChild(tmp), &set_Space) != NULL))
     {
-        snprintf(par, maxstrlen, "%s", mxmlGetText(mxmlGetFirstChild(tmp), set_Space));
+        snprintf(par, maxstrlen, "%s", mxmlGetText(mxmlGetFirstChild(tmp), &set_Space));
         return;
     }
 }
@@ -533,7 +533,7 @@ void XMLwrapper::getparstr(const string &name, char *par, int maxstrlen) const
 string XMLwrapper::getparstr(const string &name,
                              const std::string &defaultpar) const
 {
-    int *set_Space = 1;
+    int set_Space = 1;
     mxml_node_t *tmp = mxmlFindElement(node,
                                        node,
                                        "string",
@@ -547,8 +547,8 @@ string XMLwrapper::getparstr(const string &name,
     if ((mxmlGetType(mxmlGetFirstChild(tmp))) == MXML_OPAQUE && (mxmlGetElement(mxmlGetFirstChild(tmp))) != NULL)
         return mxmlGetElement(mxmlGetFirstChild(tmp));
 
-    if ((mxmlGetType(mxmlGetFirstChild(tmp))) == MXML_TEXT && (mxmlGetText(mxmlGetFirstChild(tmp), set_Space)) != NULL)
-        return mxmlGetText(mxmlGetFirstChild(tmp), set_Space);
+    if ((mxmlGetType(mxmlGetFirstChild(tmp))) == MXML_TEXT && (mxmlGetText(mxmlGetFirstChild(tmp), &set_Space)) != NULL)
+        return mxmlGetText(mxmlGetFirstChild(tmp), &set_Space);
 
     return defaultpar;
 }
