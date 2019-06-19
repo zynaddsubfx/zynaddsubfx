7a855356e2f236c0d13a2788751e2b24388fd713
michiboo <chanmickyyun@gmail.com>
2019-06-19 17:18:34 +0300

    prevent adding another watchpoint for differernt note if it is already active

--- src/Misc/Master.cpp
+++ src/Misc/Master.cpp
@@ -359,7 +359,8 @@ static const Ports automate_ports = {
 static const Ports watchPorts = {
     {"add:s", rDoc("Add synthesis state to watch"), 0,
         rBegin;
-        m->watcher.add_watch(rtosc_argument(msg,0).s);
+        if(!m->watcher.is_active(rtosc_argument(msg,0).s))
+            m->watcher.add_watch(rtosc_argument(msg,0).s);
         rEnd},
 };
 
