b08e1acc70b0e964645744ab97172db76b2b0829
Ricard Wanderlof <polluxsynth@butoba.net>
2022-03-08 00:50:34 +0100

    Update controller values when amounts changed
    Similar as was recently done for the modwheel, update the corresponding
    parameter values not just when the MIDI CC controlling the parameter
    is received, but also when the corresponding amount is changed.
    Otherwise the user experience will be confusing, as a new controller
    value must be received before a depth change can be noticed.

--- src/Params/Controller.h
+++ src/Params/Controller.h
@@ -34,11 +34,16 @@ class Controller
 
         //Controllers functions
         void setpitchwheel(int value);
+        void setpitchwheel(void);
         void setexpression(int value);
         void setpanning(int value);
+        void setpanning(void);
         void setfiltercutoff(int value);
+        void setfiltercutoff(void);
         void setfilterq(int value);
+        void setfilterq(void);
         void setbandwidth(int value);
+        void setbandwidth(void);
         void setmodwheel(int value);
         void setmodwheel(void);
         void setfmamp(int value);
@@ -48,7 +53,9 @@ class Controller
          * @param value 0-127 MIDI value (greater than 64 enables)*/
         void setportamento(int value);
         void setresonancecenter(int value);
+        void setresonancecenter(void);
         void setresonancebw(int value);
+        void setresonancebw(void);
 
 
         void setparameternumber(unsigned int type, int value); //used for RPN and NRPN's
