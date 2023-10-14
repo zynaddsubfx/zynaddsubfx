9fb8438cc551f3eb5cd3c5f472a8641ffe238e66
Friedolino <mkirchn@freenet.de>
2023-10-14 16:13:31 +0200

    fix limiting FMmodfreqlo

--- src/Synth/ADnote.cpp
+++ src/Synth/ADnote.cpp
@@ -1667,7 +1667,7 @@ inline void ADnote::ComputeVoiceOscillatorFrequencyModulation(int nvoice,
             int FMmodfreqhi = 0;
             F2I(tw[i], FMmodfreqhi);
             float FMmodfreqlo = tw[i]-FMmodfreqhi;//fmod(tw[i] /*+ 0.0000000001f*/, 1.0f);
-            if(FMmodfreqhi < 0)
+            if(FMmodfreqlo < 0)
                 FMmodfreqlo++;
 
             //carrier
