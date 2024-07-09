b8c5a80d19ed86c2efd4819429cfc32a731dd897
Friedolino <mkirchn@freenet.de>
2024-07-09 12:40:35 +0200

    add default values for new AbsTime members

--- src/Misc/Time.h
+++ src/Misc/Time.h
@@ -20,7 +20,13 @@ class AbsTime
     public:
         AbsTime(const SYNTH_T &synth)
             :frames(0),
-            s(synth){};
+            s(synth),
+            tempo(120),
+            bar(0),
+            beat(0),
+            tick(0.0f),
+            bpm(120.0f),
+            trigger(false) {};
         void operator++(){++frames;};
         void operator++(int){frames++;};
         int64_t time() const {return frames;};
