096714947b1ffff57fb0cfd3daf83a072b66d3ca
Johannes Lorenz <jlsf2013@sourceforge.net>
2026-01-04 19:52:02 +0100

    MinGW

---
+++ cmake/toolchains/mingw64.cmake
@@ -0,0 +1,17 @@
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+# MinGW-W64 Compiler
+set(CMAKE_C_COMPILER   x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+
+# Optional, aber oft sinnvoll
+set(CMAKE_RC_COMPILER  x86_64-w64-mingw32-windres)
+
+# Suche nach Libraries/Includes nur im MinGW-Root
+set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
